062dac17f274854b1879534cb380e43ce379b69d
Peter Unger <peps.unger@gmail.com>
2018-04-10 19:01:07 +0200

    Split serve into http and https

---
+++ scripts/serve-https.js
@@ -0,0 +1,26 @@
+const spdy = require('spdy');
+const pem = require('pem');
+const express = require('express');
+const compression = require('compression');
+
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+  if (err) {
+    throw err
+  }
+  spdy.createServer({
+    key: keys.serviceKey,
+    cert: keys.certificate
+  }, app).listen(8080, (error) => {
+    if (error) {
+      console.error(error);
+      return process.exit(1);
+    } else {
+      console.log('Listening https on port: 8080');
+    }
+  });
+});
--- scripts/serve.js
+++ scripts/serve.js
@@ -1,26 +1,15 @@
-const spdy = require('spdy');
-const pem = require('pem');
 const express = require('express');
 const compression = require('compression');
 
 const app = express();
-
 app.use(compression());
 app.use(express.static('dist'));
 
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-  if (err) {
-    throw err
+app.listen(8080, (error) => {
+  if (error) {
+    console.error(error);
+    return process.exit(1);
+  } else {
+    console.log('Listening on port: 8080');
   }
-  spdy.createServer({
-    key: keys.serviceKey,
-    cert: keys.certificate
-  }, app).listen(8080, (error) => {
-    if (error) {
-      console.error(error);
-      return process.exit(1);
-    } else {
-      console.log('Listening on port: 8080')
-    }
-  });
 });
