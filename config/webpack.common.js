48f8bfc6e38a46d3060a4930b0044ff110f9c03d
Peter Unger <peps.unger@gmail.com>
2018-04-11 19:37:24 +0200

    Add eslint with airbnb preset to boilerplate

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const { GenerateSW } = require('workbox-webpack-plugin');
@@ -10,34 +10,38 @@ const projectRootPath = path.join(__dirname, '..', '/');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
   },
   module: {
     rules: [
       {
+        enforce: 'pre',
         test: /\.js$/,
         exclude: [/(node_modules)/, /(dist)/],
-        use: {
-          loader: 'babel-loader'
-        }
+        use: ['eslint-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: [/(node_modules)/, /(dist)/],
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader"
-        ]
-      }
-    ]
+          'css-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin([
-      'dist/*.*'
+      'dist/*.*',
     ], {
       root: projectRootPath,
-      exclude:  ['dist/manifest.json'],
-      verbose:  true,
-      dry:      false
+      exclude: ['dist/manifest.json'],
+      verbose: true,
+      dry: false,
     }),
     new HtmlWebpackPlugin({
       cache: true,
@@ -46,28 +50,28 @@ module.exports = {
       template: 'src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "assets/[name].[hash:16].css",
-      chunkFilename: "assets/[name].[id].[chunkhash:16].css"
+      filename: 'assets/[name].[hash:16].css',
+      chunkFilename: 'assets/[name].[id].[chunkhash:16].css',
     }),
     new WebpackPwaManifest({
       name: 'React Universal Boilerplate',
       short_name: 'RUB',
       description: 'foo bar foobar',
       background_color: '#efefef',
-      display: "standalone",
-      theme_color: "#353535",
-      start_url: "/?utm_source=homescreen",
-      filename: "manifest.json",
+      display: 'standalone',
+      theme_color: '#353535',
+      start_url: '/?utm_source=homescreen',
+      filename: 'manifest.json',
       icons: [
         {
           src: path.resolve(projectRootPath, 'src/static/logo.png'),
-          sizes: [96, 128, 192, 256, 384]
+          sizes: [96, 128, 192, 256, 384],
         },
         {
           src: path.resolve(projectRootPath, 'src/static/logo.png'),
-          size: '1024x1024' // you can also use the specifications pattern
-        }
-      ]
+          size: '1024x1024', // you can also use the specifications pattern
+        },
+      ],
     }),
     new GenerateSW({
       swDest: './service-worker.js',
@@ -76,25 +80,25 @@ module.exports = {
       exclude: [/\.html$/],
       runtimeCaching: [{
         urlPattern: new RegExp('http://localhost:8080/'),
-        handler: 'networkFirst'
-      }]
-    })
+        handler: 'networkFirst',
+      }],
+    }),
   ],
   output: {
     filename: 'assets/[name].[hash:16].js',
     chunkFilename: 'assets/[name].[id].[chunkhash:16].js',
     path: path.resolve(__dirname, '../', 'dist'),
-    publicPath: '/'
+    publicPath: '/',
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         commons: {
-          name: "commons",
-          chunks: "initial",
-          minChunks: 2
-        }
-      }
-    }
-  }
+          name: 'commons',
+          chunks: 'initial',
+          minChunks: 2,
+        },
+      },
+    },
+  },
 };
